42e257574a573f07a8676a76bdc9744cdac45844
Miguel Rodrigues Carneiro <MiguelRodrigues@academicos.com>
2024-02-05 21:17:12 -0300

    Alterações desafio q4

--- src/Mainq4.java
+++ src/Mainq4.java
@@ -13,6 +13,31 @@ public class Mainq4 {
             System.out.println("Vc terá um desconto de 20% de IR!");
         }
 
+        if (salario <= 900){
+            System.out.println("Vc foi isento de IR!");
+
+        } else if ((salario > 901) && (salario <= 1500)) {
+
+            int desconto5;
+            desconto5 = (salario*100)/5;
+
+            System.out.println("Vc terá um desconto de: "+desconto5);
+
+        } else if ((salario > 1501) && (salario <= 2500)) {
+
+            int desconto5;
+            desconto5 = (salario*100)/10;
+
+            System.out.println("Vc terá um desconto de: "+desconto5);
+
+        } else {
+
+            int desconto5;
+            desconto5 = (salario*100)/20;
+
+            System.out.println("Vc terá um desconto de: "+desconto5);
+        }
+
 
     }
 }
