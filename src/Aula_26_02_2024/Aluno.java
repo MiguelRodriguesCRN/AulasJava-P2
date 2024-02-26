f12b66e19a022ed231f6f7d37a54e7fd3f7f826b
MiguelRodrigues <miguelrodriguesdev12@gmail.com>
2024-02-26 20:32:20 -0300

    Teste

--- src/Aula_26_02_2024/Aluno.java
+++ src/Aula_26_02_2024/Aluno.java
@@ -1,2 +1,10 @@
-package Aula_26_02_2024;public class Aluno {
+package Aula_26_02_2024;
+
+public class Aluno {
+
+    String nome;
+    int nota1;
+    int nota2;
+    int nota3;
+
 }
--- src/Aula_26_02_2024/Main_Aluno.java
+++ src/Aula_26_02_2024/Main_Aluno.java
@@ -1,2 +1,29 @@
-package Aula_26_02_2024;public class Main_Aluno {
+package Aula_26_02_2024;
+
+import java.util.Scanner;
+
+public class Main_Aluno {
+    public static void main(String[] args) {
+
+        Aluno a = new Aluno();
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite seu nome");
+        a.nome=sc.next();
+
+        System.out.println("Digite sua N1:");
+        a.nota1=sc.nextInt();
+
+        System.out.println("Digite sua N2:");
+        a.nota2=sc.nextInt();
+
+        System.out.println("Digite sey nome");
+        a.nota3=sc.nextInt();
+
+        int media = (a.nota1 + a.nota2 + a.nota3) / 3;
+
+        System.out.println("Seu nome é: "+ a.nome + " Sua média é: " + media);
+
+    }
 }
